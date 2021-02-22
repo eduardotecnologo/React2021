bebdcb7ca9fc53701cd987c77263ca5e3da916c0
eduardotecnologo <eduardotecnologo@hotmail.com>
2021-02-22 02:27:37 -0300

    add

---
+++ ReactJS/clone-trello/src/js/app.js
@@ -0,0 +1,14 @@
+// Scroll da Página inicial
+$(document).ready(function () {
+  $(document).on('click', 'a[href^="#"]', function (e) {
+    let id = $(this).attr('href')
+    let target = $(id)
+    if (target.length === 0) {
+      return;
+    }
+    e.preventDefault()
+    $('body, html').animate({
+      scrollTop: target.offset().top
+    });
+  });
+});
